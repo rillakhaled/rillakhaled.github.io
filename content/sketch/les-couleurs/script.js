6abea7de139c52f05339a4073770082f2a6537be
Rilla Khaled <rilla.khaled@gmail.com>
2022-04-27 12:24:12 -0400

    New post on spacetime!

--- content/sketch/les-couleurs/script.js
+++ content/sketch/les-couleurs/script.js
@@ -1,6 +1,28 @@
 
-// load the colours file
-let allColours = readTextFile("colours.json", function(text){
+const getColours = async () => {
+  try {
+    const data = await axios.get("colours.json");
+    let htmlElements = document.querySelectorAll("div.couleur");
+    let colourI = 0;
+
+    // Use a regular for of loop
+    for (let h of htmlElements) {
+      h.style.backgroundColor = data.data[colourI].rgb;
+      h.style.alpha = 0.5;
+      colourI++;
+    }
+  }
+  catch (e) {
+    console.log("load error ", e);
+  }
+}
+
+getColours();
+
+/* old and clunky way of doing this using XMLHttpRequest
+
+//load the colours file
+let allColours = readTextFile("colours.json", function (text) {
 
   let data = JSON.parse(text);
 
@@ -9,21 +31,23 @@ let allColours = readTextFile("colours.json", function(text){
   let colourI = 0;
 
   // Use a regular for-loop
-  for(let h of htmlElements) {
+  for (let h of htmlElements) {
     h.style.backgroundColor = data[colourI].rgb;
     h.style.alpha = 0.5;
     colourI++;
   }
-});
+}); 
 
 function readTextFile(file, callback) {
   let rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType("application/json");
   rawFile.open("GET", file, true);
-  rawFile.onreadystatechange = function() {
+  rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4 && rawFile.status == "200") {
       callback(rawFile.responseText);
     }
   }
   rawFile.send(null);
 }
+
+*/
