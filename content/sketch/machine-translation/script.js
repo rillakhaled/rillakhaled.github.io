7cbe7922e855943fbd93ed7114af0715393a6163
Rilla Khaled <rilla.khaled@gmail.com>
2022-05-01 14:33:12 -0400

    A new sketch plus updates to an the space-time one.

---
+++ content/sketch/machine-translation/script.js
@@ -0,0 +1,26 @@
+
+let spans = document.querySelectorAll('span');
+
+for(span of spans) {
+  span.addEventListener('click', function (e) {
+    let targetWord = e.target.innerText;
+    let URL = `https://wordsapiv1.p.rapidapi.com/words/${targetWord}/synonyms`;
+    const options = {
+      method: 'GET',
+      url: URL,
+      headers: {
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+        'X-RapidAPI-Key': '4e345ab085msh98a9faa6df16307p151054jsnb23925c30d59'
+      }
+    };
+    axios.request(options).then(function (response) {
+      let synonyms = response.data.synonyms;
+      if(synonyms.length > 0) {
+        let i = Math.floor(Math.random()*synonyms.length);
+        e.target.innerText = synonyms[i];
+      }
+    }).catch(function (error) {
+    	console.error(error);
+    });
+  })
+}
--- content/sketch/space-time/script.js
+++ content/sketch/space-time/script.js
@@ -7,73 +7,102 @@ let skyFill = '#87CEEB';
 let sunFill = '#FFA500';
 let grassFill = '#3CB371';
 
-const d = new Date();
-let hour = d.getHours();
-// document.getElementById("hour").innerHTML = hour;
-if (hour >= 22) {
+let d = new Date();
+let localHour = d.getHours();
+
+let address = document.querySelector('#where');
+
+address.addEventListener('change', function (e) {
+  e.preventDefault();
+  axios.get('http://api.positionstack.com/v1/forward', {
+    params: {
+      access_key: config.access_key,
+      query: e.target.value,
+      limit: 1,
+      timezone_module: 1
+    }
+  })
+  .then(function (response) {
+    let timezone = response.data.data[0].timezone_module;
+    let utcHour = d.getUTCHours();
+    localHour = utcHour + (timezone.offset_sec/3600);
+    localHour = localHour%24;
+    console.log(localHour);
+    renderImage();
+  }).catch(err => {
+    console.log(err);
+    // do nothing.
+  });
+});
+
+renderImage = function() {
+  if (localHour >= 22) {
     stars.show();
     sun.hide();
     skyFill = '#000066';
     grassFill = '#193300';
-}
-if (hour >= 20) {
+  }
+  else if(localHour >= 20) {
     stars.show();
     sun.hide();
     skyFill = '#000080';
     grassFill = '#193300';
-}
-else if (hour >= 18) {
+  }
+  else if(localHour >= 18) {
     stars.show();
     sun.hide();
     skyFill = '#8A2BE2';
     grassFill = '#336600';
-}
-else if (hour >= 16) {
+  }
+  else if(localHour >= 16) {
     skyFill = '#FFA07A';
     sunFill = '#FF4500';
     grassFill = '#4C9900';
     stars.hide();
-}
-else if (hour >= 12) {
+  }
+  else if(localHour >= 12) {
     stars.hide();
     skyFill = '#87CEEB';
     sunFill = '#FFA500';
     grassFill = '#4C9900';
-}
-else if (hour >= 8) {
+  }
+  else if(localHour >= 8) {
     stars.hide();
     sun.show();
     skyFill = '#3399FF';
     sunFill = '#FFA500';
     grassFill = '#4C9900';
-}
-else if (hour >= 6) {
+  }
+  else if(localHour >= 6) {
     stars.hide();
     sun.show();
     skyFill = '#0066CC';
     sunFill = '#FF9933';
     grassFill = '#4C9900';
-}
-else if (hour >= 5) {
+  }
+  else if(localHour >= 5) {
     stars.hide();
     sun.show();
     skyFill = '#004C99';
     sunFill = '#FF3333';
     grassFill = '#336600';
-}
-else if (hour >= 4) {
+  }
+  else if(localHour >= 4) {
     stars.show();
     sun.hide();
     skyFill = '#004C99';
     grassFill = '#336600';
-}
-else {
+  }
+  else {
     stars.show();
     sun.hide();
     skyFill = '#000050';
     grassFill = '#193300';
+  }
+
+  sky.fill(skyFill);
+  sun.fill(sunFill);
+  grass.fill(grassFill);
 }
 
-sky.fill(skyFill);
-sun.fill(sunFill);
-grass.fill(grassFill);
+renderImage();
