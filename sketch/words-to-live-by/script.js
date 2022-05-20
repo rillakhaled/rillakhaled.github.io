46b14d16712f7e9b7da7e7181425645c3408623b
rillakhaled <rillakhaled@users.noreply.github.com>
2022-05-20 21:33:50 +0000

    deploy: 18fe473bbf68b3d1d31a5d06be1e181017c13a67

--- sketch/words-to-live-by/script.js
+++ sketch/words-to-live-by/script.js
@@ -34,9 +34,29 @@ for(i of iWords) {
 let lastParagraph = redactedText.lastChild;
 lastParagraph.insertAdjacentHTML('beforeend', instructionsHTML);
 
+let redactedSet = document.querySelectorAll(".redacted");
+for(r of redactedSet) {
+  r.addEventListener("mouseenter", function(){
+    this.classList.remove("redacted");
+    this.classList.add("redacted-hover");
+  })
+  r.addEventListener("mouseleave", function(){
+    this.classList.remove("redacted-hover");
+    this.classList.add("redacted");
+  })
+  r.addEventListener("touchstart", function(){
+    this.classList.remove("redacted");
+    this.classList.add("redacted-hover");
+  })
+  r.addEventListener("touchend", function(){
+    this.classList.remove("redacted-hover");
+    this.classList.add("redacted");
+  })
+}
+
 let instructionSet = document.querySelectorAll(".instruction");
 for (ins of instructionSet) {
-  ins.addEventListener("mouseover", function () {
+  ins.addEventListener("mouseenter", function () {
     this.classList.remove("instruction");
     this.classList.add("read-instruction");
   })
