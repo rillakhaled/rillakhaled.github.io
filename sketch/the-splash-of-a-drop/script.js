b752905ee2eb83308616da851dea475980bf3d5d
rillakhaled <rillakhaled@users.noreply.github.com>
2022-05-26 14:24:41 +0000

    deploy: 888b3829d466faecf756fca3fc7f4e818351d539

---
+++ sketch/the-splash-of-a-drop/script.js
@@ -0,0 +1,71 @@
+class Cell {
+
+  index = 0;
+  frameFilepath = [];
+
+  constructor(start) {
+    // load up an array of frames with filepaths
+    for(let i = 1; i <= 30; i++){
+      this.frameFilepath.push(`drops/${i}.png`);
+    }
+
+    // set the index to point to the appropriate location
+    this.index = start;
+  }
+
+  advance() {
+
+    // the index advances 1 frame at a time and loops at 30
+    if(this.index < 29) {
+      this.index++;
+    }
+    else {
+      this.index = 1;
+    }
+  }
+
+  getFrame() {
+    return this.frameFilepath[this.index];
+  }
+}
+
+let frameCount = 0;
+let playing = false;
+let grid = document.querySelector(".grid-container");
+
+function tick() {
+  if(playing) {
+    frameCount++;
+    if(frameCount%10 === 0) {
+      console.log("INSIDE");
+      let drops = document.querySelectorAll(".drop");
+      for(d of drops) {
+        d.cell.advance();
+        let d_image = d.querySelector(".dropImage");
+        d_image.src = d.cell.getFrame();
+      }
+    }
+    window.requestAnimationFrame(tick);
+  }
+}
+
+
+for(let i = 1; i <= 6; i++) {
+  let dropDiv = document.createElement("div");
+  dropDiv.cell = new Cell(i);
+  dropDiv.setAttribute("class", "drop");
+  let dropI = document.createElement("img");
+  dropI.setAttribute("class", "dropImage");
+  dropI.src = dropDiv.cell.getFrame();
+  dropDiv.appendChild(dropI);
+  grid.appendChild(dropDiv);
+
+  dropDiv.addEventListener("mouseenter", function(){
+    playing = true;
+    frameCount = 0;
+    window.requestAnimationFrame(tick);
+  })
+  dropDiv.addEventListener("mouseleave", function(){
+    playing = false;
+  })
+}
